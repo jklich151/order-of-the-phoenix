f25d785cbbc7e0d803ef0b3621582a915bf0bfee
Jenny Klich <jennyklich@gmail.com>
2020-05-19 10:20:17 -0600

    Add all progress

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,16 @@
+class SearchController < ApplicationController
+  def index
+    house = params[:house]
+
+    conn = Faraday.new(url: "https://www.potterapi.com") do |faraday|
+      faraday.params["key"] = ENV["HP_KEY"]
+    end
+
+    response = conn.get("/v1/characters?&house=#{house}&orderOfThePhoenix=true")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    @members = json
+    require "pry"; binding.pry
+  end
+end
